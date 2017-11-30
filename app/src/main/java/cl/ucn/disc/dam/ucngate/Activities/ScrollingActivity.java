0284613050f453bd3678bb2bcc3dea9120135c7f
Claudio Gonzalez <pyrates_claudio@hotmail.com>
2017-11-30 17:32:05 -0400

    Se crearon los paquetes Activities y Model. Se creo la clase Persona.

--- app/src/main/java/cl/ucn/disc/dam/ucngate/Activities/ScrollingActivity.java
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/Activities/ScrollingActivity.java
@@ -1,4 +1,4 @@
-package cl.ucn.disc.dam.ucngate;
+package cl.ucn.disc.dam.ucngate.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cl.ucn.disc.dam.ucngate.R;
 
 public class ScrollingActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/Model/Persona.java
@@ -0,0 +1,10 @@
+package cl.ucn.disc.dam.ucngate.Model;
+
+/**
+ * Created by Claudio Gonzalez on 30-11-2017.
+ */
+
+public final class Persona {
+
+
+}
