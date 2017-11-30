3b63e98723d30109ba0ba45bbda41ce892e909b3
Claudio Gonzalez <pyrates_claudio@hotmail.com>
2017-11-30 18:10:23 -0400

    Se le agregaron los atributos y el método toString a la clase Persona. Se agregó Lombok y common Lang 3 al build.Gradle de app.

--- app/src/main/java/cl/ucn/disc/dam/ucngate/Model/Persona.java
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/Model/Persona.java
@@ -1,10 +1,78 @@
 package cl.ucn.disc.dam.ucngate.Model;
 
-/**
- * Created by Claudio Gonzalez on 30-11-2017.
- */
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author Claudio Gonzalez
+ */
+@Builder
 public final class Persona {
+
+    /**
+     * Rut de la {@link Persona}
+     */
+    @Getter String rut;
+
+    /**
+     * Nombre y apellido de la {@link Persona}
+     */
+    @Getter String nombre;
+
+    /**
+     * Correo electronico de la {@link Persona}
+     */
+    @Getter String email;
+
+    /**
+     * Telefono movil de la {@link Persona}
+     */
+    @Getter String telefono;
+
+    /**
+     * Numero de anexo de la {@link Persona}
+     */
+    @Getter String anexo;
+
+    /**
+     * Localizacion de la {@link Persona}: Unidad/Pabellon
+     */
+    @Getter String unidad;
+
+    /**
+     * Localizacion de la {@link Persona}: Oficina
+     */
+    @Getter String oficina;
+
+    /**
+     * Tipo de {@link Persona} dentro de los que esta determinado
+     */
+    @Getter enum tipo{académico,funcionario,apoyo,externo};
+
+    /**
+     * Cargo que tiene la {@link Persona} dentro de la universidad
+     */
+    @Getter String cargo;
+
+    /**
+     * Codigo que se le asigna a un estacionamiento dentro del campus y que es asociado a la {@link Persona}
+     */
+    @Getter int codigoEstacionamiento;
+
+
+    /**
+     * @return the String representation.
+     */
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
+
+
 
 
 }
