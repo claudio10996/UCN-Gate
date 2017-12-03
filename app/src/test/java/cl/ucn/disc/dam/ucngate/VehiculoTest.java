1a3820dac674e6d462941bf1f69c6cbecb232c2c
pablofb18 <aragorn.pablo@gmial.com>
2017-12-03 16:26:46 -0300

    - Se creo el test unitario de vehiculo. - Se agrego la libreria JUnit y AssertJ.

---
+++ app/src/test/java/cl/ucn/disc/dam/ucngate/VehiculoTest.java
@@ -0,0 +1,53 @@
+package cl.ucn.disc.dam.ucngate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import cl.ucn.disc.dam.ucngate.Model.Vehiculo;
+
+/**
+ * @author Pablo Fontecilla Busch.
+ */
+
+public final class VehiculoTest {
+
+    /**
+     * Simple test del constructor via patron builder.
+     */
+    @Test
+    public void testConstructor() {
+
+        String patente = "SG-NE-23";
+        String marca = "Ford";
+        String color = "rojo";
+        String modelo = "Explorer";
+        String anio = "2010";
+        String descripcion = "El vehículo no tiene ningun detalle interesante";
+
+        final Vehiculo vehiculo = Vehiculo.builder()
+                .patente(patente)
+                .marca(marca)
+                .color(color)
+                .modelo(modelo)
+                .anio(anio)
+                .descripcion(descripcion)
+                .build();
+
+        Assertions.assertThat(vehiculo).isNotNull();
+
+        Assertions.assertThat(vehiculo.getPatente()).isNotBlank();
+        Assertions.assertThat(vehiculo.getMarca()).isNotBlank();
+        Assertions.assertThat(vehiculo.getColor()).isNotBlank();
+        Assertions.assertThat(vehiculo.getModelo()).isNotBlank();
+        Assertions.assertThat(vehiculo.getAnio()).isNotBlank();
+        Assertions.assertThat(vehiculo.getDescripcion()).isNotBlank();
+
+        Assertions.assertThat(vehiculo.getPatente()).isEqualTo(patente);
+        Assertions.assertThat(vehiculo.getMarca()).isEqualTo(marca);
+        Assertions.assertThat(vehiculo.getColor()).isEqualTo(color);
+        Assertions.assertThat(vehiculo.getModelo()).isEqualTo(modelo);
+        Assertions.assertThat(vehiculo.getAnio()).isEqualTo(anio);
+        Assertions.assertThat(vehiculo.getDescripcion()).isEqualTo(descripcion);
+
+    }
+}
