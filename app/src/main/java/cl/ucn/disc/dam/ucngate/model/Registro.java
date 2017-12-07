4f156818e491c80b82259ab52b54ec4193738c08
Claudio Gonzalez <pyrates_claudio@hotmail.com>
2017-12-07 18:07:09 -0400

    -Se le agregó las etiquetas de DBFlow a Registro -Se intentó cambiar logo.

--- app/src/main/java/cl/ucn/disc/dam/ucngate/model/Registro.java
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/model/Registro.java
@@ -1,33 +1,52 @@
 package cl.ucn.disc.dam.ucngate.model;
 
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.joda.time.DateTime;
 
+import java.util.UUID;
+
+import cl.ucn.disc.dam.ucngate.Dao.AppDatabase;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Claudio Gonzalez
  */
 
 @Builder
+@Table(database = AppDatabase.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public final class Registro {
+
+    /**
+     * ID
+     */
+    @PrimaryKey
+    @Getter
+    UUID id;
 
     /**
      * {@link Vehiculo} que ingreso a la universidad
      */
-    @Getter Vehiculo vehiculoIngresado;
+    @Column @Getter Vehiculo vehiculoIngresado;
 
     /**
      * Fecha y hora en la que ingreso el {@link Vehiculo}
      */
-    @Getter DateTime fecha;
+    @Column @Getter DateTime fecha;
 
     /**
      * @link Entrada} por la que ingreso el {@link Vehiculo}
      */
-    @Getter Entrada entrada;
+    @Column @Getter Entrada entrada;
 
     /**
      * {@link Entrada} s disponibles
