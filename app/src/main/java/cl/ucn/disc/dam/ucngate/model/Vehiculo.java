a626984e1761a8cba6e1cfb87d2ba136af7edcd1
pablofb18 <aragorn.pablo@gmial.com>
2017-12-07 17:38:57 -0300

    - Se agrego Vehiculo a base de datos

--- app/src/main/java/cl/ucn/disc/dam/ucngate/model/Vehiculo.java
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/model/Vehiculo.java
@@ -1,10 +1,17 @@
 package cl.ucn.disc.dam.ucngate.model;
 
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import cl.ucn.disc.dam.ucngate.Dao.AppDatabase;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Vehiculo.
@@ -13,37 +20,40 @@ import lombok.Getter;
  */
 
 @Builder
+@Table(database = AppDatabase.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public final class Vehiculo {
 
     /**
      * Patente del {@link Vehiculo} con el formato AA-AA-11.
      */
-    @Getter String patente;
+    @PrimaryKey @Getter String patente;
 
     /**
      * Marca del {@link Vehiculo}.
      */
-    @Getter String marca;
+    @Column @Getter String marca;
 
     /**
      * Color del {@link Vehiculo}.
      */
-    @Getter String color;
+    @Column @Getter String color;
 
     /**
      * Modelo del {@link Vehiculo}.
      */
-    @Getter String modelo;
+    @Column @Getter String modelo;
 
     /**
      * Anio del {@link Vehiculo}
      */
-    @Getter String anio;
+    @Column @Getter String anio;
 
     /**
      * Pequeña descripcion del {@link Vehiculo}.
      */
-    @Getter String descripcion;
+    @Column @Getter String descripcion;
 
 
     /**
