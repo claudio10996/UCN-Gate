35879b7db3985c952f914e61a677a53e1bbf42ab
Claudio Gonzalez <pyrates_claudio@hotmail.com>
2017-12-27 15:47:15 -0400

    Se despliega la info en el popout, falta ingresar registro. Ya se hizo la función en controller, pero no está probada de que funciona.

--- app/src/main/java/cl/ucn/disc/dam/ucngate/Controller.java
+++ app/src/main/java/cl/ucn/disc/dam/ucngate/Controller.java
@@ -1,7 +1,12 @@
 package cl.ucn.disc.dam.ucngate;
 
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.structure.ModelAdapter;
+
+import java.util.Date;
 import java.util.List;
 
+import cl.ucn.disc.dam.ucngate.model.Registro;
 import cl.ucn.disc.dam.ucngate.model.Vehiculo;
 
 /**
@@ -12,5 +17,16 @@ public final class Controller {
 
     public List <Vehiculo> getVehiculos (){
         return null;
+    }
+
+    public void ingresarRegistro (Vehiculo vehiculo, Date fecha, Registro.Entrada entrada){
+        ModelAdapter<Registro> registroAdapter = FlowManager.getModelAdapter(Registro.class);
+        Registro regis = Registro.builder()
+                .entrada(entrada)
+                .fecha(fecha)
+                .vehiculoIngresado(vehiculo)
+                .build();
+        registroAdapter.insert(regis);
+
     }
 }
